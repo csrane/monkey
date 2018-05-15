418f3819897955b6fb01328acb2731170fa8af23
waterbeast <csrane@gmail.com>
2018-05-16 01:57:55 +0900

    함수기반 뷰(FBV)의 4가지 패턴

--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,7 @@
 #do.views.py
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 def mysum(request, numbers):
@@ -7,8 +9,10 @@ def mysum(request, numbers):
     result = sum(map(lambda s : int(s or 0) , numbers.split('/')))
     return HttpResponse(result)
  
+
 def hello(request, name, age):
     return HttpResponse('Hello, {}님은 {} 살입니다.'.format(name, age))
+
 
 def post_list1(request):
     name = '침밴지'
@@ -17,3 +21,26 @@ def post_list1(request):
     <p>그 앞에  {name} 가 서 있었다.</p>
     <p>{name}은 뚜렸한 발음으로 자유를 외쳤다.</p>
     '''.format(name=name))
+
+
+def post_list2(request):
+    name = '우랑오탄'
+    response  = render(request, 'dojo/post_list.html', {'name' : name})
+    return response
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message' : '원숭이들이 자유를 왜치며 혁명을 시작했다.',
+        'monkeys' :['개코원숭이','우랑오탄','침밴치','그랜드고릴라','여우원숭이','비비원숭이'],
+    },json_dumps_params={'ensure_ascii':False})
+
+
+def excel_download(request):
+    #filepath = '/Users/Waterbeast/monkey/PROFORMA.xls'
+    filepath = os.path.join(settings.BASE_DIR, 'PROFORMA.xls')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type="application/vnd.ms-excel")
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
