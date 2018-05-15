f99ab22fc2c2094d2b1855339ec82f608fc9b523
waterbeast <csrane@gmail.com>
2018-05-16 00:58:38 +0900

    dojo.views.post_list1 생성- Function Base View 예시

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+    url(r'^list1/$', views.post_list1),
 ] 
--- dojo/views.py
+++ dojo/views.py
@@ -7,5 +7,13 @@ def mysum(request, numbers):
     result = sum(map(lambda s : int(s or 0) , numbers.split('/')))
     return HttpResponse(result)
  
-def hello(requset, name, age):
+def hello(request, name, age):
     return HttpResponse('Hello, {}님은 {} 살입니다.'.format(name, age))
+
+def post_list1(request):
+    name = '침밴지'
+    return HttpResponse('''
+    <h1>원숭이들이 반란을 일으켰다</h1>
+    <p>그 앞에  {name} 가 서 있었다.</p>
+    <p>{name}은 뚜렸한 발음으로 자유를 외쳤다.</p>
+    '''.format(name=name))
