0655e945377115ea4de8ed612d41e5a830305180
waterbeast <csrane@gmail.com>
2018-05-15 17:16:21 +0900

    dojo.views.mysum 에서 x, y, z 인자 처리

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,5 +1,8 @@
+from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    
+    url(r'^sum/(?P<x>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
 ] 
