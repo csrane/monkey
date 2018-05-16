b07e30bab62ee77d0cd3019b54b7dae3753e8c4f
waterbeast <csrane@gmail.com>
2018-05-16 23:46:22 +0900

    blog.models.py 에 author 필드추가/ 마이그래션 / 필수항목입력 - Anonymous / 마이그래이트

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+    author = models.CharField(max_length=20, verbose_name = '작성자') 
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = "제목을 입력하시오. 최대 100자 내외")
     content = models.TextField(verbose_name = '내용')
     tags = models.CharField(max_length=100, blank=True)
