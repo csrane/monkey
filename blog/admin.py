ddbe4b514fb94f700199e22b1b9f764d8abbe6a8
waterbeast <csrane@gmail.com>
2018-05-17 01:20:09 +0900

    blog.admin.PostAdmin에 list_display 에 content_size 를 정의해서 추가/ 커스텀 수행

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,13 @@
 #blog/admin.py
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id' , 'title' , 'created_at' , 'updated_at' , 'author']
+    list_display = ['id' , 'title' ,'content_size', 'created_at' , 'updated_at' , 'author']
+
+
+    def content_size(self, post):
+        return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
+    content_size.short_description = "내용글자수"
