fff3f3671af324fcfcd07ecd5b6f0ec44f5d8445
waterbeast <csrane@gmail.com>
2018-05-16 22:16:31 +0900

    blog.models.Post 모델 생성/ 마이그래이션/어드민 등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+#blog/models.py
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name = '제목', help_text = "제목을 입력하시오. 최대 100자 내외")
+    content = models.TextField(verbose_name = '내용')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
