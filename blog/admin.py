82fada3b92fb1d1888c99f8eb14d68dac59c7e07
waterbeast <csrane@gmail.com>
2018-05-17 02:01:31 +0900

    blog.models.Post 모델에 status 추가하고 / 이를 처리하는 Custom Admin Action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,31 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id' , 'title' ,'content_size', 'created_at' , 'updated_at' , 'author']
-
+    list_display = ['id' , 'title' , 'status' ,'content_size', 'created_at' , 'updated_at' , 'author']
+    actions = [
+        'make_published',
+        'make_draft',
+        'make_withdrawn'
+    ]
 
     def content_size(self, post):
         return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     content_size.short_description = "내용글자수"
+
+    
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 Published 상태로 변경했습니다.'.format(updated_count))
+    make_published.short_description = '지정포스팅 발행'
+
+
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경했습니다.'.format(updated_count))
+    make_draft.short_description = '지정포스팅 대기'
+
+
+    def make_withdrawn(self, request, queryset):
+        updated_count = queryset.update(status='w')
+        self.message_user(request, '{}건의 포스팅을 Withdrawn 상태로 변경했습니다.'.format(updated_count))
+    make_withdrawn.short_description = '지정포스팅 숨김'
--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,12 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+    
     author = models.CharField(max_length=20, verbose_name = '작성자') 
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = "제목을 입력하시오. 최대 100자 내외")
     content = models.TextField(verbose_name = '내용')
@@ -16,5 +22,6 @@ class Post(models.Model):
     lnglat = models.CharField(max_length=50, blank=True, 
         validators = [lnglat_validator],
         help_text='경도/위도 포멧으로 입력하시오')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
