8447c4342f3db98db2913cfb2e1241c357761223
waterbeast <csrane@gmail.com>
2018-05-18 01:58:47 +0900

    쿼리셋 만들어서 blog.views.post_list 에서 리스트생성

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
         ('p', 'Published'),
         ('w', 'Withdrawn'),
     )
-    
+
     author = models.CharField(max_length=20, verbose_name = '작성자') 
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = "제목을 입력하시오. 최대 100자 내외")
     content = models.TextField(verbose_name = '내용')
@@ -25,3 +25,6 @@ class Post(models.Model):
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
