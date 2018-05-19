851217f78f9948ee9f4233aebc87ecb44e5dab81
waterbeast <csrane@gmail.com>
2018-05-19 12:04:05 +0900

    blog.models.Post 모델에 tag_set 을 blank로 지정

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
         validators = [lnglat_validator],
         help_text='경도/위도 포멧으로 입력하시오')
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    tag_set = models.ManyToManyField('Tag')
+    tag_set = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -46,4 +46,4 @@ class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.name 
