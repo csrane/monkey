73fba18633280d2af0e6124195b4ba486eeed018
waterbeast <csrane@gmail.com>
2018-05-20 20:43:16 +0900

    accounts/profile.html 셋팅 / urls.py -> view.py -> templates/accounts/layout.html ->templates/accounts/profile.html / 레이아웃 상속 예제

--- monkey/settings.py
+++ monkey/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'monkey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'monkey', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
