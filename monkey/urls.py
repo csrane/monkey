8e1a37a8f98fac825c43d1aa82d11f7703420081
waterbeast <csrane@gmail.com>
2018-05-22 20:44:53 +0900

    media 파일 서빙 지원

--- monkey/urls.py
+++ monkey/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include,url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect, resolve_url
 
@@ -16,6 +17,8 @@ urlpatterns = [
     url(r'^dojo/', include('dojo.urls', namespace='dojo')),
     url(r'^shop/',include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
