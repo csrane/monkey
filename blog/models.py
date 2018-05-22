58f25fdc9fe30112e4d402a736d7f3426e42aade
waterbeast <csrane@gmail.com>
2018-05-23 01:46:33 +0900

    imagekit 을 활용 / 원본을 유지하면서 썸네일 필드관리

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.forms import ValidationError
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import Thumbnail
 
 
 def lnglat_validator(value):
@@ -22,6 +24,11 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = "제목을 입력하시오. 최대 100자 내외")
     content = models.TextField(verbose_name = '내용')
     photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d' )
+    photo_thumbnail = ImageSpecField(source='photo',
+                                    processors=[Thumbnail(300,300)],
+                                    format='JPEG',
+                                    options={'qualit':60})
+
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True, 
         validators = [lnglat_validator],
