2bd628a4e1135c58a97f0b4329e9fdd087299caa
waterbeast <csrane@gmail.com>
2018-05-23 21:04:28 +0900

    blog 앱 CBV : ListView 적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.views.generic import CreateView
+from django.views.generic import ListView, CreateView
 from .models import Post
+
+
+post_list = ListView.as_view(model=Post, paginate_by=10)
 
 #blog.forms.py 가 있다 가정
 class PostForm(forms.ModelForm):
