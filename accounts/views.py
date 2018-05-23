2b825a687d076bd97800111fa11909aae3b3aebe
waterbeast <csrane@gmail.com>
2018-05-24 00:20:08 +0900

    회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
 #accounts/views.py
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL) # defualt : "account/login"
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form' : form,
+    })
+
+
 
 def profile(request):
     return render(request, 'accounts/profile.html')
