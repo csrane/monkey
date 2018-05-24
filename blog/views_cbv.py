1189ab5b5dca25784a82ffaee8d1c32faf287755
waterbeast <csrane@gmail.com>
2018-05-24 21:44:11 +0900

    prefetch_related : after 샘플(CBV 버전)

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -4,12 +4,17 @@ from django.views.generic import ListView, CreateView, DetailView, UpdateView, D
 from .models import Post
 
 
-post_list = ListView.as_view(model=Post, paginate_by=10)
+class PostListView(ListView):
+    model = Post
+    queryset = Post.objects.all().prefetch_related('tag_set','comment_set')
+    paginate_by = 10
+
+post_list = PostListView.as_view()
 
 post_detail = DetailView.as_view(model=Post)
 
 post_new = CreateView.as_view(model=Post)
-
+ 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
 
 post_delete = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list')) 
